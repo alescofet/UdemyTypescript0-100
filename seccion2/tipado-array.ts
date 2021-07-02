f667cfaa70bffe9d365d66aafdc7d95b0a01dd24
alescofet <alex.escofet96@gmail.com>
2021-07-02 17:32:02 +0200

    tipados array and tupla done

---
+++ seccion2/tipado-array.ts
@@ -0,0 +1,5 @@
+let numeros: number[] = [1,2,3,4]
+let palabras: string[] = ["Alex", "Pedro", "Manuel"]
+
+let coleccionnumeros: Array<number> = [1,2,3,4]
+let coleccionpalabras: Array<string> = ["Alex", "Pedro", "Manuel"]
---
+++ seccion2/tipado-tupla.ts
@@ -0,0 +1,3 @@
+let tupla:[number,string,boolean]
+
+tupla = [23, "hola", true]
